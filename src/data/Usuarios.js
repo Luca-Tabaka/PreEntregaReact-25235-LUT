06e89382fb1036b4753076b0cd04854cc801997b
Lu <lucauriel.tabaka@estudiantes.unahur.edu.ar>
2025-10-23 22:46:23 -0300

    Agrego data con los datos simulados de usuarios

---
+++ src/data/Usuarios.js
@@ -0,0 +1,11 @@
+//archivo de datos de usuarios simulados para probar el login
+export const usuarios = [ // de momento el login sera solo para admins
+    {
+        usuario: "admin",
+        contraseña: "admin123",
+        foto: "https://randomuser.me/api/portraits/men/84.jpg",
+        nombre: "Juan",
+        apellido: "Pérez",
+        email: "juan.perez@gmail.com"
+    }
+]
